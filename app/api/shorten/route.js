a5ee2c265867fddceb92295915b8c239e0f8a70d
AbhiRam-beep <abhiramrayadurgam12345@gmail.com>
2025-02-23 08:38:49 +0530

    inital commit

---
+++ app/api/shorten/route.js
@@ -0,0 +1,13 @@
+import { nanoid } from "nanoid";
+import { NextResponse } from "next/server";
+
+
+export async function POST(req) {
+  const { url } = await req.json();
+  if (!url) {
+    return NextResponse.json({ error: 'URL is requred' }, { status: 400 })
+  }
+  const surl = nanoid(7);
+  const shortenedURL = `https://localhost:3000/${surl}`
+  return NextResponse.json({ newurl: shortenedURL });
+}
--- app/page.js
+++ app/page.js
@@ -1,101 +1,71 @@
-import Image from "next/image";
+'use client'
+import { db } from "@/lib/firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { useState } from "react";
 
-export default function Home() {
+const Home = () => {
+
+  const [shorturl, setShorturl] = useState(null);
+  const [inputURL, setInputURL] = useState(null);
+
+  const getShortURL = async (url) => {
+    try {
+      const res = await fetch("/api/shorten", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ url }),
+      });
+
+      if (!res.ok) throw new Error("Error generating shortened URL");
+
+      const data = await res.json();
+      setShorturl(data.newurl);
+      return data.newurl;
+    } catch (error) {
+      console.log("error: " + error);
+      return false;
+    }
+
+
+
+  }
+
+  const addURL = async (oldurl, newurl) => {
+    try {
+      const docRef = await addDoc(collection(db, "urls"), { oldurl, newurl });
+      console.log("document id:" + docRef.id);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!inputURL) return;
+
+    const success = await getShortURL(inputURL);
+    if (shorturl) {
+      await addURL(inputURL, shorturl)
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputURL} placeholder="enter URL" onChange={(e) => setInputURL(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {shorturl && (
+          <div>
+            <p>Shortened URL: <a href={shorturl} target="_blank">{shorturl}</a></p>
+          </div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home;
